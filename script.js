c3da5c83b2337778fde97cfd06c2b65a590b39bd
Zect <57027411+Zect3279@users.noreply.github.com>
2023-07-25 22:38:31 +0900

    力こそパワー!

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const codeName = document.getElementById('code_name');
+const reco = document.getElementById('reco');
+reco.textContent = ""
+
+const videoOut = document.getElementById('video_out');
+videoOut.classList.add("hidden-video");
+
 const video = document.getElementById('input');
 const canvas = document.getElementById('output');
 const ctx = canvas.getContext('2d');
@@ -6,6 +13,10 @@ const config = {
 };
 
 const hands = new Hands(config);
+
+function wait(delayInMS) {
+  return new Promise((resolve) => setTimeout(resolve, delayInMS));
+}
 
 const camera = new Camera(video, {
   onFrame: async () => {
@@ -25,16 +36,106 @@ hands.setOptions({
 stream = document.getElementById('output').captureStream();
 recorder = new MediaRecorder(stream, {mimeType:'video/webm;codecs=vp9'});
 // レコーダー
+// コード
+const guitarChords = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+const ChordType = ['M', 'M7', '7', '6', 'aug', 'm', 'mM7', 'm7', 'm6', 'm7b5', 'add9', 'sus4', '7sus4', 'dim7'];
 
+// ランダムなインデックスを生成する関数
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// ランダムなギターコードを取得して出力する関数
+function getRandomGuitarChord() {
+  const randomIndex1 = getRandomIndex(guitarChords.length);
+  const randomIndex2 = getRandomIndex(ChordType.length);
+  return guitarChords[randomIndex1] + ChordType[randomIndex2];
+}
+// コード
+
+var chordWeb = document.getElementById('downloadlink');
 document.getElementById('start').addEventListener('click', () => {
+  video.classList.remove("hidden-video");
+  canvas.classList.remove("hidden-video");
+  videoOut.classList.add('hidden-video');
+  chordWeb.classList.add('hidden-video');
+  document.getElementById('start').classList.add('hidden-video')
+  document.getElementById('replay').classList.add('hidden-video')
+  codeName.textContent = getRandomGuitarChord()
   camera.start();
-  recorder.start();
+  reco.textContent = "録画開始まで...10"
+  wait(900).then(() => {
+    reco.textContent = "録画開始まで...9"
+    wait(900).then(() => {
+      reco.textContent = "録画開始まで...8"
+      wait(900).then(() => {
+        reco.textContent = "録画開始まで...7"
+        wait(900).then(() => {
+          reco.textContent = "録画開始まで...6"
+          wait(900).then(() => {
+            reco.textContent = "録画開始まで...5"
+            wait(900).then(() => {
+              reco.textContent = "録画開始まで...4"
+              wait(900).then(() => {
+                reco.textContent = "録画開始まで...3"
+                wait(900).then(() => {
+                  reco.textContent = "録画開始まで...2"
+                  wait(900).then(() => {
+                    reco.textContent = "録画開始まで...1"
+                    wait(900).then(() => {
+                      reco.textContent = "記録開始"
+                      recorder.start();
+                      wait(900).then(() => {
+                        reco.textContent = "記録中 : 1"
+                        wait(900).then(() => {
+                          reco.textContent = "記録中 : 2"
+                          wait(900).then(() => {
+                            reco.textContent = "記録中 : 3"
+                            wait(900).then(() => {
+                              reco.textContent = "記録中 : 4"
+                              wait(900).then(() => {
+                                reco.textContent = "記録終了: 10秒後に再生します。必ず「movie.webm」として保存してください"
+                                recorder.stop();
+                                wait(10000).then(() => {
+                                  video.classList.add("hidden-video");
+                                  canvas.classList.add("hidden-video");
+                                  videoOut.classList.remove('hidden-video');
+                                  videoOut.src = "movie.webm"
+                                  videoOut.load()
+                                  videoOut.play()
+                                  wait(6000).then(() => {
+                                    reco.textContent = "再生終了 : startで再開"
+                                    chordWeb.classList.remove('hidden-video');
+                                    chordWeb.href = "https://muuu.jp/chords/#" + codeName.textContent.toLowerCase().replace('#', '3')
+                                    document.getElementById('start').classList.remove('hidden-video')
+                                    document.getElementById('replay').classList.remove('hidden-video')
+                                  })
+                                })
+                              })
+                            })
+                          })
+                        })
+                      })
+                    })
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+  })
 });
 
-document.getElementById('stop').addEventListener('click', () => {
-  camera.stop();
-  recorder.stop();
-});
+document.getElementById('replay').addEventListener('click', () => {
+  videoOut.currentTime = 0
+  videoOut.play()
+})
+// document.getElementById('stop').addEventListener('click', () => {
+//   camera.stop();
+//   recorder.stop();
+// });
 
 hands.onResults(results => {
   ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -54,7 +155,8 @@ hands.onResults(results => {
   }
 });
 
-var anchor = document.getElementById('downloadlink');
+// var anchor = document.getElementById('downloadlink');
+const anchor = document.createElement('a');
 // 録画が終了したときの処理
 recorder.ondataavailable = function(e) {
   var videoBlob = new Blob([e.data], { type: e.data.type });
@@ -62,4 +164,7 @@ recorder.ondataavailable = function(e) {
   anchor.download = 'movie.webm';
   anchor.href = blobUrl;
   anchor.style.display = 'block';
+  document.body.appendChild(anchor);
+  anchor.click();
+  URL.revokeObjectURL(blobUrl);
 }
